314422626f09b6d1557fd6b00bf680a97a2075ab
nullice <ui@nullice.com>
2015-02-08 11:04:29 +0800

    V1.0

--- Release_hinaLayer_exe/test.bat
+++ Release_hinaLayer_exe/test.bat
@@ -1,10 +1,6 @@
 
-
-
-
-rem:hinaLayer_v1.0.exe en_eo_mask "1.png" "mask.png" "[en_mask]1_out.png" 3
-
-rem:hinaLayer_v1.0.exe  read_eo "[en_mask]1_out.png"  3
+hinaLayer_v1.0.exe en_eo_mask "1.png" "mask.png" "[en_mask]1_out.png" 3
+hinaLayer_v1.0.exe  read_eo "[en_mask]1_out.png"  3
 
 
 hinaLayer_v1.0.exe en_dtf_mask "1.png" "mask.png" "[en_dtf]1_out.png" 3
