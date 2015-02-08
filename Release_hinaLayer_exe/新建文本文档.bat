603f5dcfa326354790e6373c49da298f44cf031f
nullice <ui@nullice.com>
2015-02-08 10:38:08 +0800

    058

--- Release_hinaLayer_exe/新建文本文档.bat
+++ Release_hinaLayer_exe/新建文本文档.bat
@@ -2,9 +2,13 @@
 
 
 
-hinaLayer_v1.0.exe en_eo_mask "1.png" "mask.png" "[en_mask]1_out.png" 3
+rem:hinaLayer_v1.0.exe en_eo_mask "1.png" "mask.png" "[en_mask]1_out.png" 3
 
-hinaLayer_v1.0.exe  read_eo "[en_mask]1_out.png"  3
+rem:hinaLayer_v1.0.exe  read_eo "[en_mask]1_out.png"  3
+
+
+hinaLayer_v1.0.exe en_dtf_mask "1.png" "mask.png" "[en_dtf]1_out.png" 3
+hinaLayer_v1.0.exe  read_dtf "[en_dtf]1_out.png"  3
 
 pause
 
